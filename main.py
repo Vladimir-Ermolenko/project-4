fc3fd59429ce4a82aab191bbebf9c84d548a3e6f
Vladimir-Ermolenko <v.ermolenko@g.nsu.ru>
2019-11-05 01:24:25 +0700

    description added

---
+++ main.py
@@ -0,0 +1,2 @@
+# The program to solve the typical economic exercises connected with finding the equilibrium positions on the market
+# (+ the sum of taxes paid, the new market positions after taxes introduction)
