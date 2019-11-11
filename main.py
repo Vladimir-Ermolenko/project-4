5bf8bccbb57813021f582aedbb1db915d3284030
Vladimir-Ermolenko <v.ermolenko@g.nsu.ru>
2019-11-11 11:54:43 +0700

    final-final version

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 # The program to solve the typical economic exercises connected with finding the equilibrium positions on the market
 
-import math
-
 print('Эта программа поможет Вам в решении простейших экономических задач, в которых даны функции покупателя и '
       'и продавца и нужно найти равновесную цену и объём')
 
@@ -13,14 +11,13 @@ qdms = qdf.find('-')
 qsms = qsf.find('-')
 
 while qdms == 0 or qsms == 0:
-      print('Ошибка, введите данные повторно')
-      qd = input('Введите линейную функцию спроса в формате a - bP : ')
-      qs = input('Введите линейную функцию предложения в формате c + dP : ')
-      qdf = qd.replace(' ', '')
-      qsf = qs.replace(' ', '')
-      qdms = qdf.find('-')
-      qsms = qsf.find('-')
-
+    print('Ошибка, введите данные повторно')
+    qd = input('Введите линейную функцию спроса в формате a - bP : ')
+    qs = input('Введите линейную функцию предложения в формате c + dP : ')
+    qdf = qd.replace(' ', '')
+    qsf = qs.replace(' ', '')
+    qdms = qdf.find('-')
+    qsms = qsf.find('-')
 
 lnqdf = len(qdf)
 lnqsf = len(qsf)
@@ -28,18 +25,23 @@ qsps = qsf.find('+')
 
 qda = int(qdf[0:qdms])
 if qdf[qdms + 1:lnqdf] == 'p':
-      qdb = 1
+    qdb = 1
 else:
-      qdb = int(qdf[qdms + 1:lnqdf - 1])
+    qdb = int(qdf[qdms + 1:lnqdf - 1])
 
 qsc = int(qsf[0:qsps])
 if qsf[qsps + 1:lnqsf] == 'p':
-      qsd = 1
+    qsd = 1
 else:
-      qsd = int(qsf[qsps + 1:lnqsf - 1])
+    qsd = int(qsf[qsps + 1:lnqsf - 1])
 
-pe = math.ceil(qda - qsc)/(qdb + qsd)
-qe = qda - qdb * pe
+pe = round((qda - qsc) / (qdb + qsd), 1)
+qe = round(qda - qdb * pe, 1)
 
 print('\n', end='')
-print('При заданных условиях равновесная цена равна', pe, 'а равновесный объём', qe)
+if pe >= 0 or qe >=0:
+      print('При заданных условиях равновесная цена равна', pe, 'а равновесный объём', qe)
+elif pe >= 0 and qe >=0:
+      print('При заданных условиях равновесная цена равна', pe, 'а равновесный объём', qe)
+else:
+      print('Ошибка в условиях задачи')
